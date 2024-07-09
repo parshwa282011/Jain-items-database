2f43b9d96c99607fa2c1ca2d8728f7f375dba8d6
parshwa282011 <99104612+parshwa282011@users.noreply.github.com>
2024-07-09 18:53:00 +0000

    firebase fix

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-var admin = require("firebase-admin");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-var serviceAccount = {
-    "type": "service_account", 
-    "project_id": process.env.PROJECT_ID,
-    "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
-    "client_email": "firebase-adminsdk-pcawi@jain-project-39725.iam.gserviceaccount.com",
-    "client_id": "118236614911499753603",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-pcawi%40jain-project-39725.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA34L0BtbPm0EGnJkOSJ97farFblZrfMCI",
+  authDomain: "jain-project-39725.firebaseapp.com",
+  databaseURL: "https://jain-project-39725-default-rtdb.firebaseio.com",
+  projectId: "jain-project-39725",
+  storageBucket: "jain-project-39725.appspot.com",
+  messagingSenderId: "900081551595",
+  appId: "1:900081551595:web:623004a5756a39b88516ae",
+  measurementId: "G-MEJRCKVD4Z"
+};
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-console.log(admin.database)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
